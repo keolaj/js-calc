63e686de8a0faa98373b1625e82b402a7ff7d382
Keola <keolajarvegren@gmail.com>
2022-07-31 23:05:12 -0700

    js functionality. todo refactor code without using global variables. or use react.'

--- index.js
+++ index.js
@@ -27,3 +27,48 @@ function operate(op, num1, num2) {
 	}
 }
 
+var eqArr = [];
+var currNum = 0;
+
+function updateEqText() {
+	let eqString = ""
+	eqArr.forEach(el => {
+		eqString += " ";
+		eqString += el;
+	})
+	document.querySelector(".equation").textContent = eqString;
+}
+
+function updateDisplay(num) {
+	document.querySelector(".output").textContent = num;
+}
+
+function pressNum(num) {
+	currNum = (currNum * 10) + num;
+	updateDisplay(currNum);
+}
+
+function pressOp(op) {
+	eqArr.push(currNum);
+	eqArr.push(op);
+	updateEqText();
+	currNum = 0;
+}
+function solve() {
+	
+}
+
+document.querySelectorAll(".num").forEach(node => {
+	node.addEventListener("click", () => {
+		pressNum(parseInt(node.textContent))
+	})
+})
+document.querySelectorAll(".op").forEach(node => {
+	node.addEventListener("click", () => {
+		pressOp(node.textContent);
+	})
+})
+document.querySelector(".eq").addEventListener("click", () => {
+	eqArr.push(currNum);
+
+})
